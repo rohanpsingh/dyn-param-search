a89f9c63707609ac2c9e9085d709a899c1505e18
Rohan P. Singh <rohan565singh@gmail.com>
2024-02-01 18:06:33 +0900

    minor improvements to print_log.py

--- utils/print_log.py
+++ utils/print_log.py
@@ -1,23 +1,27 @@
-import sys
+import argparse
 import mc_log_ui
 import matplotlib.pyplot as plt
 
-JOINT_ID = 1
-
-def parse(logfile, outfile):
+def parse(logfile, outfile, jointid):
     print("Parsing: {}".format(logfile))
     log = mc_log_ui.read_log(logfile)
 
     # get start time of 'Demo' state
     exec_main = log['Executor_Main']
     demo_start = exec_main.index('JointCalibState')
+    demo_end = len(exec_main) - exec_main[::-1].index('JointCalibState') - 1
 
     with open(outfile, 'w') as f:
-        for t in range(17000):
-            l = log['qIn_' + repr(JOINT_ID)][t]
+        for t in range(demo_start, demo_end):
+            l = log['qIn_' + repr(jointid)][t]
             f.write(repr(l) + "\n")
 
 if __name__=='__main__':
-    in_fn = sys.argv[1]
-    out_fn = sys.argv[2]
-    parse(in_fn, out_fn)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--in-file", type=str, required=True)
+    parser.add_argument("--out-file", type=str, required=True)
+    parser.add_argument("--joint-id", type=int, required=True)
+    args = parser.parse_args()
+
+    parse(args.in_file, args.out_file, args.joint_id)
